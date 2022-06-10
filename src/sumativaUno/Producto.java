16118a9236f4aac7f37b3fd3d7bcbcac6429586d
vestalia007 <107218705+vestalia007@users.noreply.github.com>
2022-06-09 22:05:37 -0400

    Agregar disponibilidad
    Se agrega disponibilidad de stock de producto (solo verdadero o falso)

--- src/sumativaUno/Producto.java
+++ src/sumativaUno/Producto.java
@@ -4,15 +4,17 @@ public class Producto {
 	private int id;
 	private int precio;
 	private String nombre;
+	private int disponible;
 		
 	public Producto() {
 		
 	}
 
-	public Producto(int id, int precio, String nombre) {
+	public Producto(int id, int precio, String nombre, int disponible) {
 		this.id = id;
 		this.precio = precio;
 		this.nombre = nombre;
+		this.disponible = disponible;
 	}
 
 	public int getId() {
@@ -38,10 +40,24 @@ public class Producto {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
+	public int isDisponible() {
+		if (disponible > 0 ) {
+		return disponible;
+		}
+		else {
+		System.out.println("No hay disponibilidad de producto");
+		}
+		return disponible;
+			
+	}
+	public void setDisponible(int disponible) {
+		this.disponible = disponible;
+	}
+	
 	@Override
 	public String toString() {
-		return "ID: " + id + ", Precio=" + precio + ", Nombre=" + nombre ;
+		return "ID: " + id + ", Precio=" + precio + ", Nombre=" + nombre+ ",Disponible= "+ disponible ;
 	}
 	
 	
